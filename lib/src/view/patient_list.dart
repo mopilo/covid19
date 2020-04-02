5ed6d8ae94273fa062e102e4ab3b9639ae52f206
mopah emma <mopahemma@gmail.com>
2020-04-02 01:54:57 +0100

    display patient list

--- lib/src/view/patient_list.dart
+++ lib/src/view/patient_list.dart
@@ -1,4 +1,5 @@
 import 'package:covid_19/src/core/bloc/home_bloc.dart';
+import 'package:covid_19/src/core/model/patient_model.dart';
 import 'package:flutter/material.dart';
 
 class PatientList extends StatefulWidget{
@@ -18,7 +19,46 @@ class _PatientListState extends State<PatientList> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(),
+      body: Container(
+        child: StreamBuilder(
+          stream: homebloc.patientData,
+          builder: (context, AsyncSnapshot<PatientModel> snapshot) {
+            if(snapshot.hasData){
+              var patient = snapshot.data.result;
+              return ListView.builder(
+                itemCount: patient.length,
+                shrinkWrap: true,
+              itemBuilder: (BuildContext context, int index) { 
+                return Container(
+                  margin: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 0.4),
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text( 'Name: ' + patient[index].firstname + " " + patient[index].lastname),
+                      Text('Gender: ' + patient[index].gender),
+                      Text('City: ' + patient[index].city),
+                      Text('State: ' + patient[index].state),
+                      Text('Lat: ' + patient[index].lat),
+                      Text('Lng: ' + patient[index].lng),
+                    ],
+                  ),
+                );
+               },);
+            }
+            else if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            }
+            return Center(child: CircularProgressIndicator());
+            
+          }
+        ),
+      ),
     );
   }
 }
